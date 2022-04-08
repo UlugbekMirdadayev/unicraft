573f829a4310a43e1e00182b3baed0d7013f880d
100798f <farhodbek.nomonov@mail.ru>
2022-04-08 15:07:48 +0500

    update src/components/rate/index.js

--- src/components/rate/index.js
+++ src/components/rate/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 function Rate() {
   return (
-    <div>Rate</div>
-  )
+    <div className="rate">
+      <h1>Тарифы</h1>
+      <p>
+        Сравните пакеты предложений и выберите подходящие возможности и
+        стоимость
+      </p>
+      <div className="tariff_blocks_period">
+        <p>Квартал</p>
+        <div className="tariff_blocks_period_switch">
+          <div className="switch"></div>
+        </div>
+        <div className="tariff_blocks_period_year">
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Rate
+export default Rate;
