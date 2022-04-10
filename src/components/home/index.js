455032edf0b5419af75951e825f6dca0562a4932
Ulugbek Mirdadayev <ulugbekmirdadayev1211@gmail.com>
2022-04-10 11:39:57 +0500

    update home.scss and src/components/home/index.js

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -25,6 +25,14 @@ import ProblemThree from "../../assets/img/problem3.png";
 import ProblemFour from "../../assets/img/problem4.png";
 import ProblemFive from "../../assets/img/problem5.png";
 import ProblemSix from "../../assets/img/problem6.png";
+import AdvanOne from "../../assets/img/slider-advan1.png";
+import AdvanSecond from "../../assets/img/slider-advan2.png";
+import AdvanThird from "../../assets/img/slider-advan3.png";
+import AdvanFourth from "../../assets/img/slider-advan4.png";
+import AdvanFifth from "../../assets/img/slider-advan5.png";
+import AdvanSixth from "../../assets/img/slider-advan6.png";
+import AdvanSeventh from "../../assets/img/slider-advan7.png";
+import AdvanEighth from "../../assets/img/slider-advan8.png";
 
 const Liders = [
   {
@@ -81,7 +89,7 @@ const AnyProblems = [
   {
     img: Problem,
     title: "Онлайн-обучение для клиентов",
-    text: "Зарабатывайте на предоставлении доступа к курсам и программам",
+    text: "Зарабатывайте на предоставлении доступа к курсам",
   },
   {
     img: ProblemTwo,
@@ -89,27 +97,143 @@ const AnyProblems = [
     text: "Быстро вводите в должность новых сотрудников",
   },
   {
+    img: ProblemThree,
     title: "Обучение ассортименту и продажам",
     text: "Оперативно сообщайте о новинках и увеличивайте доход",
   },
   {
-    img: ProblemThree,
+    img: ProblemFour,
     title: "Систематизация и масштабирование",
     text: "Расширяйте бизнес и занимайте новые территории",
   },
   {
-    img: ProblemFour,
+    img: ProblemFive,
     title: "Регулярная аттестация сотрудников",
     text: "Проверяйте знания для соблюдения единых стандартов",
   },
   {
-    img: ProblemFive,
+    img: ProblemSix,
     title: "Полная автоматизация HR процессов",
     text: "Решайте все кадровые задачи в одной системе",
   },
 ];
 
+const Advantages = [
+  {
+    pagination: [
+      { name: "Загрузка материалов", id: 1 },
+      { name: "Конструктор упражнений", id: 2 },
+      { name: "Управление обучением", id: 3 },
+      { name: "Сводная аналитика", id: 4 },
+      { name: "HR функции", id: 5 },
+      { name: "Визуальные", id: 6 },
+      { name: "Технологические", id: 7 },
+      { name: "Сопровождение", id: 8 },
+    ],
+    card: [
+      {
+        id: 1,
+
+        title: "Конструктор упражнений",
+        list: [
+          "Полноценный встроенный редактор уроков",
+          "Поддержка всех популярных форматов файлов",
+          "Видеохостинги и конструкторы web-страниц",
+          "Защита контента от копирования и скачивания",
+          "Интерактивные курсы в формате SCORM",
+        ],
+        img: AdvanOne,
+      },
+      {
+        id: 2,
+        title: "Конструктор упражнений",
+        list: [
+          "Широкий выбор видов тестирования",
+          "Практические задания с ручной проверкой",
+          "Продуманная система управления баллами",
+          "Случайная выборка из готового банка",
+          "Таймер и управление попытками сдачи",
+        ],
+        img: AdvanSecond,
+      },
+      {
+        id: 3,
+        title: "Управление обучением",
+        list: [
+          "Объединение курсов в программы и каталоги",
+          "Распределение пользователей по группам",
+          "Гибкое управление доступом и назначениями",
+          "Интеграция с вебинарными сервисами",
+          "Готовые курсы от внешних провайдеров",
+        ],
+        img: AdvanThird,
+      },
+      {
+        id: 4,
+        title: "Сводная аналитика",
+        list: [
+          "Более 20 показателей эффективности",
+          "Групповые и индивидуальные отчеты",
+          "Свободный импорт и экспорт данных",
+          "Сбор оценок и отзывов по курсам",
+          "Разработка новых отчетов на заказ",
+        ],
+        img: AdvanFourth,
+      },
+      {
+        id: 5,
+        title: "HR функции",
+        list: [
+          "Автоматическое назначение по должности",
+          "Отдельный кабинет для руководителей",
+          "Аттестации с блокировкой доступа к теории",
+          "Управление дедлайнами и сроками доступа",
+          "Синхронизация с кадровыми системами",
+        ],
+        img: AdvanFifth,
+      },
+      {
+        id: 6,
+        title: "Визуальные",
+        list: [
+          "Привлекательный удобный интерфейс",
+          "Разнообразные инструменты геймификации",
+          "Персональное брендирование платформы",
+          "Сертификаты по индивидуальному образцу",
+          "White-label мобильное приложение",
+        ],
+        img: AdvanSixth,
+      },
+      {
+        id: 7,
+        title: "Технологические",
+        list: [
+          "Современный стек технологий",
+          "Коробочная или облачная версия",
+          "Безлимитное хранилище данных",
+          "Разработка новых функций по запросу",
+          "Открытое API для интеграций",
+        ],
+        img: AdvanSeventh,
+      },
+      {
+        id: 8,
+        title: "Сопровождение",
+        list: [
+          "Оперативная заботливая поддержка",
+          "Аудит текущей системы обучения",
+          "Составление структуры материалов",
+          "Заказная разработка курсов и тестов",
+          "Перенос обучения с другой платформы",
+        ],
+        img: AdvanEighth,
+      },
+    ],
+  },
+];
+
 function Home() {
+  const [active, setActive] = React.useState(1);
   const options = {
     scale: 1,
     speed: 1000,
@@ -205,13 +329,49 @@ function Home() {
           {AnyProblems.map((__res, i) => {
             return (
               <div className="card__" key={i + __res?.title}>
-                <img src={__res?.img} alt="" />
+                <div className="card__img">
+                  <img src={__res?.img} alt="" />
+                </div>
                 <div className="__card_title">{__res?.title}</div>
                 <div className="__card_text">{__res?.text}</div>
                 <button>Подробнее</button>
               </div>
             );
           })}
+        </div>
+      </div>
+      <div className="container_section">
+        <div className="container_section_body">
+          <div className="container_section_title">
+            <h2>Преимущества платформы</h2>
+            <p className="hugetext">
+              Все, что нужно для полноценного управления обучением, мы уместили
+              в удобные и эргономичные интерфейсы
+            </p>
+          </div>
+          <div className="container_section_contents">
+            {Advantages.map((__res, i) => {
+              return (
+                <div className="card___" key={i + __res?.title}>
+                  <div className="card__pagination">
+                    {__res?.pagination.map((_it) => {
+                      return (
+                        <div
+                          className={`card__pagination_item ${
+                            active === _it?.id ? "active" : ""
+                          }`}
+                          onClick={() => setActive(_it?.id)}
+                          key={_it?.id}
+                        >
+                          {_it?.name}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </main>
